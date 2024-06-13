928103a888bf7a1c865094f36f6e10a0b83d4d84
Annas Saker <a.saker-ext@kreactive.com>
2024-06-13 11:14:08 +0200

    Initial Commit

---
+++ NutriQuest/NutriQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriQuestApp.swift
+//  NutriQuest
+//
+//  Created by Annas on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NutriQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
