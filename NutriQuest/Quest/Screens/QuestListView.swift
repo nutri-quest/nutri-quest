5e85374e68aceb99fcf2cb864f7c71c86b9e54bb
Annas Saker <a.saker-ext@kreactive.com>
2024-06-13 11:48:21 +0200

    Création ecran quête
    Closes #1

---
+++ NutriQuest/Quest/Screens/QuestListView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestListView.swift
+//  NutriQuest
+//
+//  Created by Annas on 13/06/2024.
+//
+
+import SwiftUI
+
+struct QuestListView: View {
+    var body: some View {
+        Text("Liste des quêtes")
+    }
+}
+
+struct QuestListView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestListView()
+    }
+}
